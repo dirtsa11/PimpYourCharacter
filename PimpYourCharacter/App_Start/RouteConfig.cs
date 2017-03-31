b82561ffe3517c091edc2e4103556fc261d6be14
SS39 <chadoc39@live.fr>
2017-03-31 10:26:53 -0400

    Essai Routes Nez

--- PimpYourCharacter/App_Start/RouteConfig.cs
+++ PimpYourCharacter/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace PimpYourCharacter
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Nez",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "nez", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
