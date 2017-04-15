a46b50f1aa419d6abbfc00caf44098422586dc62
AlexisCornet <alexiscornet@hotmail.fr>
2017-04-15 15:34:40 -0400

    test

--- PimpYourCharacter/Controllers/EthnieController.cs
+++ PimpYourCharacter/Controllers/EthnieController.cs
@@ -17,6 +17,9 @@ namespace PimpYourCharacter.Controllers
         // GET: Ethnie
         public ActionResult Index()
         {
+
+            SearchController test = new SearchController();
+            test.searchBouche(-1, "", -1, -1, -1, -1);
             return View(db.ethnie.ToList());
         }
 
--- PimpYourCharacter/Controllers/SearchController.cs
+++ PimpYourCharacter/Controllers/SearchController.cs
@@ -25,11 +25,16 @@ namespace PimpYourCharacter.Controllers
             var requeteRecherche = from bou in db.bouche select bou;
 
             requeteRecherche = (id_bouche == -1) ? requeteRecherche : requeteRecherche.Where(bou => bou.id_bouche == id_bouche);
-            requeteRecherche = (forme.CompareTo("") == 0 || forme == null) ? requeteRecherche : requeteRecherche.Where(bou => bou.forme.Contains(forme) ;
+            requeteRecherche = (forme.CompareTo("") == 0 || forme == null) ? requeteRecherche : requeteRecherche.Where(bou => bou.forme.Contains(forme));
             requeteRecherche = (largeur == -1) ? requeteRecherche : requeteRecherche.Where(bou => bou.largeur == largeur);
             requeteRecherche = (hauteur == -1) ? requeteRecherche : requeteRecherche.Where(bou => bou.hauteur == hauteur);
             requeteRecherche = (profondeur == -1) ? requeteRecherche : requeteRecherche.Where(bou => bou.profondeur == profondeur);
             requeteRecherche = (id_couleur == -1) ? requeteRecherche : requeteRecherche.Where(bou => bou.id_couleur == id_couleur);
+
+            foreach(var t in requeteRecherche)
+            {
+                Console.WriteLine("{0} | {1} | {2} | {3} | {4} | {5}", t.id_bouche,t.forme,t.largeur,t.hauteur,t.profondeur, t.id_couleur);
+            }
 
             return View(requeteRecherche);
         }
