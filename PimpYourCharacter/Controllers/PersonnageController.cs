71367351aa98d9dd525f95cb3bbed25001ed0fe2
SS39 <chadoc39@live.fr>
2017-04-16 20:37:11 -0400

    Edit Personnage OK (sauf ternaire)

--- PimpYourCharacter/Controllers/PersonnageController.cs
+++ PimpYourCharacter/Controllers/PersonnageController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PimpYourCharacter.Models;
+using PimpYourCharacter.ViewModel;
 
 namespace PimpYourCharacter.Controllers
 {
@@ -76,7 +77,7 @@ namespace PimpYourCharacter.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            personnage personnage = db.personnage.Find(id);
+            /*personnage personnage = db.personnage.Find(id);
             if (personnage == null)
             {
                 return HttpNotFound();
@@ -86,7 +87,69 @@ namespace PimpYourCharacter.Controllers
             ViewBag.id_genre = new SelectList(db.genre, "id_genre", "label", personnage.id_genre);
             ViewBag.id_vbas = new SelectList(db.vbas, "id_vbas", "label", personnage.id_vbas);
             ViewBag.id_vhaut = new SelectList(db.vhaut, "id_vhaut", "label", personnage.id_vhaut);
+            
             return View(personnage);
+             */
+
+            var personnageViewModel = new PersonnageViewModel
+            {
+                personnage = db.personnage.Include(i => i.accessoire).Include(i => i.arme).Include(i => i.bouclier).Include(i => i.vmain).Include(i => i.vpied).Include(i => i.vtete).First(i => i.id_personnage == id),
+            };
+
+            if (personnageViewModel.personnage == null)
+            {
+                return HttpNotFound();
+            }
+
+            var allAccessoiresList = db.accessoire.ToList();
+            personnageViewModel.allAccessoires = allAccessoiresList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_accessoire.ToString()
+            });
+
+            var allArmesList = db.arme.ToList();
+            personnageViewModel.allArmes = allArmesList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_arme.ToString()
+            });
+
+            var allBoucliersList = db.bouclier.ToList();
+            personnageViewModel.allBoucliers = allBoucliersList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_bouclier.ToString()
+            });
+
+            var allVMainsList = db.vmain.ToList();
+            personnageViewModel.allVMains = allVMainsList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_vmain.ToString()
+            });
+
+            var allVPiedsList = db.vpied.ToList();
+            personnageViewModel.allVPieds = allVPiedsList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_vpied.ToString()
+            });
+
+            var allVTetesList = db.vtete.ToList();
+            personnageViewModel.allVTetes = allVTetesList.Select(o => new SelectListItem
+            {
+                Text = o.label,
+                Value = o.id_vtete.ToString()
+            });
+
+            ViewBag.id_corps = new SelectList(db.corps, "id_corps", "id_corps", personnageViewModel.personnage.id_corps);
+            ViewBag.id_ethnie = new SelectList(db.ethnie, "id_ethnie", "id_ethnie", personnageViewModel.personnage.id_ethnie);
+            ViewBag.id_genre = new SelectList(db.genre, "id_genre", "id_genre", personnageViewModel.personnage.id_genre);
+            ViewBag.id_vbas = new SelectList(db.vbas, "id_vbas", "id_vbas", personnageViewModel.personnage.id_vbas);
+            ViewBag.id_vhaut = new SelectList(db.vhaut, "id_vhaut", "id_vhaut", personnageViewModel.personnage.id_vhaut);
+
+            return View(personnageViewModel);
         }
 
         // POST: Personnage/Edit/5
@@ -94,21 +157,127 @@ namespace PimpYourCharacter.Controllers
         // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id_personnage,nom,age,id_ethnie,id_genre,id_vbas,id_vhaut,id_corps")] personnage personnage)
+        public ActionResult Edit(PersonnageViewModel personnageView)
+        {
+            if (personnageView == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (ModelState.IsValid)
+            {
+                var personnageToUpdate = db.personnage.Include(i => i.accessoire).Include(i => i.arme).Include(i => i.bouclier).Include(i => i.vmain).Include(i => i.vpied).Include(i => i.vtete).First(i => i.id_personnage == personnageView.personnage.id_personnage); ;
+
+                if (TryUpdateModel(personnageToUpdate, "Personnage", new string[] { "id_personnage,nom,age,id_ethnie,id_genre,id_vbas,id_vhaut,id_corps" }))
+                {
+                    var newAccessoires = db.accessoire.Where(m => personnageView.SelectedAccessoires.Contains(m.id_accessoire)).ToList();
+                    var updatedAccessoires = new HashSet<int>(personnageView.SelectedAccessoires);
+                    foreach (accessoire acc in db.accessoire)
+                    {
+                        if (!updatedAccessoires.Contains(acc.id_accessoire))
+                        {
+                            personnageToUpdate.accessoire.Remove(acc);
+                        }
+                        else
+                        {
+                            personnageToUpdate.accessoire.Add((acc));
+                        }
+                    }
+
+                    var newArmes = db.arme.Where(m => personnageView.SelectedArmes.Contains(m.id_arme)).ToList();
+                    var updatedArmes = new HashSet<int>(personnageView.SelectedArmes);
+                    foreach (arme arm in db.arme)
+                    {
+                        if (!updatedArmes.Contains(arm.id_arme))
+                        {
+                            personnageToUpdate.arme.Remove(arm);
+                        }
+                        else
+                        {
+                            personnageToUpdate.arme.Add((arm));
+                        }
+                    }
+
+                    var newBoucliers = db.bouclier.Where(m => personnageView.SelectedBoucliers.Contains(m.id_bouclier)).ToList();
+                    var updatedBoucliers = new HashSet<int>(personnageView.SelectedBoucliers);
+                    foreach (bouclier bou in db.bouclier)
+                    {
+                        if (!updatedBoucliers.Contains(bou.id_bouclier))
+                        {
+                            personnageToUpdate.bouclier.Remove(bou);
+                        }
+                        else
+                        {
+                            personnageToUpdate.bouclier.Add((bou));
+                        }
+                    }
+
+                    var newVMains = db.vmain.Where(m => personnageView.SelectedVMains.Contains(m.id_vmain)).ToList();
+                    var updatedVMains = new HashSet<int>(personnageView.SelectedVMains);
+                    foreach (vmain vm in db.vmain)
+                    {
+                        if (!updatedVMains.Contains(vm.id_vmain))
+                        {
+                            personnageToUpdate.vmain.Remove(vm);
+                        }
+                        else
+                        {
+                            personnageToUpdate.vmain.Add((vm));
+                        }
+                    }
+
+                    var newVPieds = db.vpied.Where(m => personnageView.SelectedVPieds.Contains(m.id_vpied)).ToList();
+                    var updatedVPieds = new HashSet<int>(personnageView.SelectedVPieds);
+                    foreach (vpied vp in db.vpied)
+                    {
+                        if (!updatedVPieds.Contains(vp.id_vpied))
+                        {
+                            personnageToUpdate.vpied.Remove(vp);
+                        }
+                        else
+                        {
+                            personnageToUpdate.vpied.Add((vp));
+                        }
+                    }
+
+                    var newVTetes = db.vtete.Where(m => personnageView.SelectedVTetes.Contains(m.id_vtete)).ToList();
+                    var updatedVTetes = new HashSet<int>(personnageView.SelectedVTetes);
+                    foreach (vtete vt in db.vtete)
+                    {
+                        if (!updatedVTetes.Contains(vt.id_vtete))
+                        {
+                            personnageToUpdate.vtete.Remove(vt);
+                        }
+                        else
+                        {
+                            personnageToUpdate.vtete.Add((vt));
+                        }
+                    }
+
+                    db.Entry(personnageToUpdate).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_corps = new SelectList(db.corps, "id_corps", "id_corps", personnageView.personnage.id_corps);
+            ViewBag.id_ethnie = new SelectList(db.ethnie, "id_ethnie", "id_ethnie", personnageView.personnage.id_ethnie);
+            ViewBag.id_genre = new SelectList(db.genre, "id_genre", "id_genre", personnageView.personnage.id_genre);
+            ViewBag.id_vbas = new SelectList(db.vbas, "id_vbas", "id_vbas", personnageView.personnage.id_vbas);
+            ViewBag.id_vhaut = new SelectList(db.vhaut, "id_vhaut", "id_vhaut", personnageView.personnage.id_vhaut);
+
+            return View(personnageView);
+        }
+
+        /*public ActionResult Edit([Bind(Include = "id_personnage,nom,age,id_ethnie,id_genre,id_vbas,id_vhaut,id_corps")] PersonnageViewModel personnageViewModel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(personnage).State = EntityState.Modified;
+                db.Entry(personnageViewModel.personnage).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.id_corps = new SelectList(db.corps, "id_corps", "id_corps", personnage.id_corps);
-            ViewBag.id_ethnie = new SelectList(db.ethnie, "id_ethnie", "label", personnage.id_ethnie);
-            ViewBag.id_genre = new SelectList(db.genre, "id_genre", "label", personnage.id_genre);
-            ViewBag.id_vbas = new SelectList(db.vbas, "id_vbas", "label", personnage.id_vbas);
-            ViewBag.id_vhaut = new SelectList(db.vhaut, "id_vhaut", "label", personnage.id_vhaut);
-            return View(personnage);
-        }
+
+            return View(personnageViewModel);
+        }*/
 
         // GET: Personnage/Delete/5
         public ActionResult Delete(int? id)
---
+++ PimpYourCharacter/ViewModel/PersonnageViewModel.cs
@@ -0,0 +1,104 @@
+﻿using PimpYourCharacter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PimpYourCharacter.ViewModel
+{
+    public class PersonnageViewModel
+    {
+        public personnage personnage { get; set; }
+        public IEnumerable<SelectListItem> allAccessoires { get; set; }
+        public IEnumerable<SelectListItem> allArmes { get; set; }
+        public IEnumerable<SelectListItem> allBoucliers { get; set; }
+        public IEnumerable<SelectListItem> allVMains { get; set; }
+        public IEnumerable<SelectListItem> allVPieds { get; set; }
+        public IEnumerable<SelectListItem> allVTetes { get; set; }
+
+        private List<int> _selectedAccessoires;
+        public List<int> SelectedAccessoires
+        {
+            get
+            {
+                if (_selectedAccessoires == null)
+                {
+                    _selectedAccessoires = personnage.accessoire.Select(m => m.id_accessoire).ToList();
+                }
+                return _selectedAccessoires;
+            }
+            set { _selectedAccessoires = value; }
+        }
+
+        private List<int> _selectedArmes;
+        public List<int> SelectedArmes
+        {
+            get
+            {
+                if (_selectedArmes == null)
+                {
+                    _selectedArmes = personnage.arme.Select(m => m.id_arme).ToList();
+                }
+                return _selectedArmes;
+            }
+            set { _selectedArmes = value; }
+        }
+
+        private List<int> _selectedBoucliers;
+        public List<int> SelectedBoucliers
+        {
+            get
+            {
+                if (_selectedBoucliers == null)
+                {
+                    _selectedBoucliers = personnage.bouclier.Select(m => m.id_bouclier).ToList();
+                }
+                return _selectedBoucliers;
+            }
+            set { _selectedBoucliers = value; }
+        }
+
+        private List<int> _selectedVMains;
+        public List<int> SelectedVMains
+        {
+            get
+            {
+                if (_selectedVMains == null)
+                {
+                    _selectedVMains = personnage.vmain.Select(m => m.id_vmain).ToList();
+                }
+                return _selectedVMains;
+            }
+            set { _selectedVMains = value; }
+        }
+
+        private List<int> _selectedVPieds;
+        public List<int> SelectedVPieds
+        {
+            get
+            {
+                if (_selectedVPieds == null)
+                {
+                    _selectedVPieds = personnage.vpied.Select(m => m.id_vpied).ToList();
+                }
+                return _selectedVPieds;
+            }
+            set { _selectedVPieds = value; }
+        }
+
+        private List<int> _selectedVTetes;
+        public List<int> SelectedVTetes
+        {
+            get
+            {
+                if (_selectedVTetes == null)
+                {
+                    _selectedVTetes = personnage.vtete.Select(m => m.id_vtete).ToList();
+                }
+                return _selectedVTetes;
+            }
+            set { _selectedVTetes = value; }
+        }
+    }
+}
